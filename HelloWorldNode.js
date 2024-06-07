97e1a812957b2ccba94781b067adefa47095d95f
Somesh Rao Coka <111784343+SomeshRao007@users.noreply.github.com>
2024-06-07 21:18:16 +0700

    Create HelloWorldNode.js

---
+++ HelloWorldNode.js
@@ -0,0 +1,18 @@
+const http = require('node:http');
+
+const listener = function (request, response) {
+   const currentDate = new Date();
+   const dateTimeString = currentDate.toLocaleString();
+
+   response.writeHead(200, {'Content-Type': 'text/html'});
+   response.end(`
+     <h2 style="text-align: center;">Hello World</h2>
+     <p style="text-align: center;">Current Date and Time: ${dateTimeString}</p>
+   `);
+};
+
+const server = http.createServer(listener);
+server.listen(3000);
+
+// To print the message
+console.log('Server running at http://<public_ip>:3000/');
